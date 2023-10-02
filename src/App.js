0a0c3033f7d004900a1660eaf28b66e13fede8ed
KlayvemG <klayvemguik@gmail.com>
2023-10-02 13:15:15 -0300

    avancos4

--- src/App.js
+++ src/App.js
@@ -284,7 +284,7 @@ function App() {
                 aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])}
                 />
               <section className="times">
-                <h1>Minha organização</h1>
+                <h1>Trilhas disponíveis!</h1>
                 {times.map((time, indice) => 
                   <Time 
                     aoFavoritar={resolverFavorito}
--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -5,8 +5,6 @@ const Rodape = () => {
     return (
     <footer className="footer">
        <p>Desenvolvido por <a href="https://www.linkedin.com/in/klayvemguimaraes/">Klayvem Guimarães</a> e <a href="https://www.linkedin.com/in/rayssabuarque/">Rayssa Buarque</a> 🚀✨</p>
-    
-        
     </footer>
     )
 }
